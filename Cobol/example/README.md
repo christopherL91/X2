9eefc58f03020dfb45bc9dee61ff76ea216d4631
Christopher Lillthors <christopher.lillthors@gmail.com>
2015-01-04 17:36:47 +0100

    Added example COBOL program

---
+++ Cobol/example/README.md
@@ -0,0 +1,5 @@
+Install COBOL compiler on mac using "brew install gnu-cobol"
+
+cobc -x hello.cob
+=> ./hello
+=> Hello World!
---
+++ Cobol/example/hello.cob
@@ -0,0 +1,6 @@
+      *Example COBOL program
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. hello_world.
+       PROCEDURE DIVISION.
+       DISPLAY "Hello World!".
+       STOP RUN.
