8494b20b42d99dc347038931b5b34d01a8e0074c
Christopher Lillthors <christopher.lillthors@gmail.com>
2015-01-04 16:49:13 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Contains all the code for X2 progp14
---
+++ progp.erl
@@ -0,0 +1,25 @@
+%Several functions for progp X2
+-module(progp).
+-date('4/1-15').
+-license('MIT').
+-export([bernoulli/1,factorial/1,binomial/2,bernoulliseq/1]).
+
+factorial(N) ->
+    tail_factorial(N,1).
+
+tail_factorial(0,M) ->
+    M;
+tail_factorial(N,M) when N > 0 ->
+    tail_factorial(N-1,N*M).
+
+% n is always 0 in the recursive definition.
+bernoulli(0) ->
+    1;
+bernoulli(M) when is_integer(M) ->
+    0 - lists:sum([binomial(M,K)* (bernoulli(K)/(M-K+1)) || K <- lists:seq(0,M-1)]).
+
+bernoulliseq(M) when is_integer(M) ->
+    [bernoulli(N) || N <- lists:seq(0,M)].
+
+binomial(N,K) when is_number(N), is_number(K) ->
+    factorial(N) div (factorial(N-K) * factorial(K)).
